803073576768731ff42362809bfaf9d7bd2c0d0b
fedeeduffey-blip <fedee.duffey@gmail.com>
2026-09-02 14:25:37 -0300

    Actualizacion final TP1 - Legajo 53343

--- src/App.java
+++ src/App.java
@@ -1,27 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
 public class App {
     public static void main(String[] args) {
         Sala sala1 = new Sala(1, "Aula Magna");
-        EventoUniversitario evento1 = new EventoUniversitario(
-                    "EVT-1",
-                    "Jornada de Programación",
-                    10000,
-                    false
-            );
-    EventoUniversitario copiaEvento1 = new EventoUniversitario(evento1);
+        EventoUniversitario eventoPrincipal = new EventoUniversitario(
+                "EVT-1",
+                "Jornada de Tecnología",
+                10000,
+                false);
 
-    evento1.asignarSala(sala1);
-    evento1.crearActividad(101, "Taller de Java OO", 30,"taller");
-    evento1.crearActividad(102, "Conferencia de Algoritmos", 50,"charla");
-    System.out.println("EVENTO ORIGINAL");
-    evento1.mostrarDatos();
+        Charla charla = new Charla(1, "Introducción a Algoritmos", "Alberto Cortez", 40);
+        Taller taller = new Taller(2, "Taller de Java", true, 15);
 
-    System.out.println();
+        eventoPrincipal.asignarSala(sala1);
+        eventoPrincipal.crearActividad(1, "Introducción a Algoritmos", 40, "charla");
+        eventoPrincipal.crearActividad(2, "Taller de Java", 15, "taller");
 
-    System.out.println("COPIA DEL EVENTO");
-    copiaEvento1.mostrarDatos();
+        EventoUniversitario copiaeventoPrincipal = new EventoUniversitario("EVT-2", "Jornada de Tecnología Copia", 10000, false);
 
-    System.out.println("Eventos creados: "+ EventoUniversitario.getCantidadEventos());
+        eventoPrincipal.asignarSala(sala1);
+
+        Estudiante e1 = new Estudiante("53343", "Federica Duffey");
+        Estudiante e2 = new Estudiante("58731", "Tomás Herrera");
+        Estudiante e3 = new Estudiante("53345", "Guadalupe Ríos");
+
+        List<Estudiante> estudiantes = new ArrayList<>();
+        estudiantes.add(e1);
+        estudiantes.add(e2);
+        estudiantes.add(e3);
+
+
+        charla.inscribir(e1);
+        charla.inscribir(e2);
+
+        taller.inscribir(e2);
+        taller.inscribir(e3);
+
+        System.out.println("EVENTO PRINCIPAL");
+        eventoPrincipal.mostrarDatos();
+
+        System.out.println();
+
+
+        System.out.println("COPIA DEL EVENTO");
+    copiaeventoPrincipal.mostrarDatos();
+
+        System.out.println("Eventos creados: "+ EventoUniversitario.getCantidadEventos());
      }
 }
